b91d74bc0b8f12962d5e9507a8be796973f750b1
ANDRES FELIPE RAMIREZ USUGA <andresramirez229848@correo.itm.edu.co>
2023-03-31 23:54:08 -0500

    Add User Photo

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -56,6 +56,8 @@ builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConne
 builder.Services.AddTransient<SeedDb>();
 builder.Services.AddScoped<IApiService, ApiService>();
 builder.Services.AddScoped<IUserHelper, UserHelper>();
+builder.Services.AddScoped<IFileStorage, FileStorage>();
+
 
 builder.Services.AddIdentity<User, IdentityRole>(x =>
 {
